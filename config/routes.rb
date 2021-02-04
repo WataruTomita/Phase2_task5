b4bf9017c7764977829d5d72fa3d8d908c38580c
WataruTomita <tomi.symphonia@gmail.com>
2021-02-04 12:07:45 +0900

    [finish]task5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'search/search'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => "homes#top"
   get "home/about" => "homes#about"
+
+  get '/search', to: 'search#search'
 
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
   resources :users, only: [:index, :show, :edit, :update] do
